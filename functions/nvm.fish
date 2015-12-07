afd78c1971ec5a8dc3f3a76eabf7dda3f92baefe
Ryan Zeigler <ryan.zeigler@taillight.com>
2015-12-07 13:09:58 -0600

    Improve variable handling.

--- functions/nvm.fish
+++ functions/nvm.fish
@@ -1,26 +1,42 @@
 function nvm --description "Configure the nvm command"
-    if set -q $NVM_PATH
+    # Always strip NVM_BIN from user_paths if it is defined
+    if set -q NVM_BIN; and set -q fish_user_paths
         set -l local_paths
-        # Attempt to strip NVM_PATH from fish_user_paths
         for path in $fish_user_paths
-            if test $NVM_PATH != $path
+            if test $NVM_BIN != $path
                 set local_paths $path $local_paths
             end
         end
-        set -x fish_user_paths $local_paths
+        set -xU fish_user_paths $local_paths
     end
 
+    # If deactivating, unset NVM_BIN so that it won't be readded to environment
+    if test $argv[1] = "deactivate"
+        set -e NVM_BIN
+    end
+    # nvm doesn't seem to pick up the current version across shells without this
+    if set -q NVM_BIN;
+        set -x PATH $NVM_BIN $PATH
+    end
     bass source ~/.nvm/nvm.sh ';' nvm $argv
 
-    # Replay interesting variables as universal
-    set NVM_BIN_ $NVM_BIN
-    set -e NVM_BIN
-    set -xU NVM_BIN $NVM_BIN_
+    # Replay as universal for use at top
+    if set -q NVM_BIN;
+        # Remove NVM_BIN from PATH
+        set -l stripped_path
+        for path in $PATH
+            if test $path != $NVM_BIN
+                set stripped_path $path $stripped_path
+            end
+        end
+        set PATH $stripped_path
 
-    set NVM_PATH_ $NVM_PATH
-    set -e NVM_PATH
-    set -xU NVM_PATH $NVM_PATH_
+        # Set NVM_BIN universally
+        set NVM_BIN_ $NVM_BIN
+        set -e NVM_BIN
+        set -xU NVM_BIN $NVM_BIN_
 
-    # Reset user_paths
-    set -xU fish_user_paths $NVM_BIN $fish_user_paths
+        # Add NVM_BIN back to user_paths
+        set -xU fish_user_paths $NVM_BIN $fish_user_paths
+    end
 end
